ae3811581879bfcf03fac344eab5c9767fffdbbe
Kakomo <kakomo2@gmail.com>
2021-01-20 23:16:37 -0300

    Class 1 - Lesson 2

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:bytebank/components/BalanceCard.dart';
+import 'package:bytebank/models/balance.dart';
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatelessWidget {
@@ -10,7 +11,7 @@ class Dashboard extends StatelessWidget {
       ),
       body: Align(
         alignment: Alignment.topCenter,
-          child: BalanceCard()),
+          child: BalanceCard(Balance(110.0))),
     );
   }
 }
